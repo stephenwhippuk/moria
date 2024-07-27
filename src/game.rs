ff6e4b2a1809e764ce714cf36b9b88c4957d71c7
Stephen Whipp <stephenwhippuk@gmail.com>
2024-07-27 23:34:55 +0100

    Added main Loop, command interpretor and display module

---
+++ src/game.rs
@@ -0,0 +1,61 @@
+use std::io;
+use crate::map::Map;
+use crate::player::Player;
+use crate::command::Command;
+use crate::command::get_command;
+use crate::display::display_location;
+pub struct Game {
+    player: Player,
+    map: Map,
+}
+
+impl Game {
+    pub fn new() -> Game {
+        Game {
+            player: Player::new(0),
+            map: Map::new(),
+        }
+    }
+    
+    pub fn initialise(&mut self) {
+        
+    }
+
+    fn display(&self){
+        let current = self.map.get_location(self.player.location_id);
+        match current{
+            Some(current) => display_location(&current),
+            None => println!("Location not found"),
+        }
+    }
+
+    pub fn run(&mut self){
+        let mut exit = false;
+        while !exit {
+            // for now we'll just do the input in here but this should be moved out into its own module shortly
+            self.display();
+
+            let command = get_command();
+            match command {
+                Some(Command::Quit) => {
+                    exit = true;
+                },
+                Some(Command::Move(direction)) => {
+                    let new_location = self.map.move_to(self.player.location_id, Some(direction));
+                    match new_location {
+                        Some(new_location) => {
+                            self.player.location_id = new_location;
+                        },
+                        None => {
+                            println!("You can't go that way");
+                        }
+                    }
+                },
+                None => {
+                    println!("Unknown command");
+                }
+            }
+        }
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,14 @@
-use crate::map::Direction;
-//use crate::map::Location_Dto;
-use crate::map::Map;
-use crate::gameobj::GameObject;
+use crate::game::Game;
 
 pub mod map;
 pub mod gameobj;
+pub mod player;
+pub mod game;
+pub mod command;
+pub mod display;
 
 fn main() {
-    let map = Map::new();
-    let current_location: usize = 0;
-    let mut loc = map.get_location(current_location);
-    match(loc){
-        Some(loc) => {
-            println!("{}", loc.name);
-            println!("{}", loc.description);
-            println!("{}", loc.exits);
-        },
-        None => {
-            println!("Location not found");
-        }
-    }
-    // attempt to move to the north
-    let next_location = map.move_to(current_location, Some(Direction::North));
-    match(next_location){
-        Some(next_location) => {
-            loc = map.get_location(next_location);
-            match loc {
-                Some(loc) => {
-                    println!("{}", loc.name);
-                    println!("{}", loc.description);
-                    println!("{}", loc.exits);
-                },
-                None => {
-                    println!("Location not found");
-                }
-            }
-        },
-        None => {
-            let is_locked = map.is_locked(current_location, Direction::North);
-            let is_closed = map.is_closed(current_location, Direction::North);
-            if is_locked {
-                println!("The door is locked");
-            } else if is_closed {
-                println!("The door is closed");
-            } else {
-                println!("You can't go that way!");
-            }
-        }
-    }
-    let mut loc = map.get_location(0);
-
-    // let mut loc = map.get_location(0);
-    // println!("{}", loc.name);
-    // println!("{}", loc.description);
-    // println!("{}", loc.exits);
-    // let next_location = map.move_to(0, Direction::South);
-    // if next_location.0 == -1 {
-    //     println!("You can't go that way!");
-    // } else {
-    //     loc = map.get_location(next_location.1);
-    //     println!("{}", loc.name);
-    //     println!("{}", loc.description);
-    //     println!("{}", loc.exits);
-    // }
-    // let next_location = map.move_to(next_location.1, Direction::South);
-    // if next_location.0 == -1 {
-    //     println!("You can't go that way!");
-    // } else {
-    //     loc = map.get_location(next_location.1);
-    //     println!("{}", loc.name);
-    //     println!("{}", loc.description);
-    //     println!("{}", loc.exits);
-    // }
+    let mut game = Game::new();
+    game.initialise();
+    game.run();
 }
